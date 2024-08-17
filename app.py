3a107f65d1a16f402035a0def618d2d448948472
Zaid <zaid1442011@icloud.com>
2024-08-17 11:22:44 +0300

    Added error handling

--- app.py
+++ app.py
@@ -13,12 +13,19 @@ server=os.getenv('cloud_host')
 contact_info=os.getenv('contact')
 
 #Connect to Turbowarp's cloud server or another cloud server
-turbo_conn = scratch3.TwCloudConnection(project_id=scratch_project, username=username, cloud_host=server, purpose="A bot that syncs Scratch variables to another cloud server", contact=contact_info)
+try:
+    turbo_conn = scratch3.TwCloudConnection(project_id=scratch_project, username=username, cloud_host=server, purpose="A bot that syncs Scratch variables to another cloud server", contact=contact_info)
+except:
+    print("Couldn't connect to the cloud server. Closing the program.")
+    exit()
 
 set_vars = None
 while True:
     #Get a dictionary of the cloud variables on Scratch
     variables = scratch3.get_cloud(scratch_project)
+    if variables == {} or variables == None:
+        print("Something went wrong while retrieving the cloud variable list or there are no cloud variables in the project. Closing.")
+        exit()
     #Sets these variables in Turbowarp
     if set_vars == variables: continue
     for var in variables:
